d62cb4be2342a96a497af9e344a2b018b7b85ef3
Dennis Imhof <dennis.imhof@stud.h-da.de>
2021-10-29 09:35:25 +0200

    ADD basic package structure

---
+++ hda_fits/fits.py
@@ -0,0 +1,7 @@
+"""FITS file helper functions
+
+This module contains functionality helping with the FITS file I/O.
+This means reading in FITS tables, extracting coordinates and
+meta information as well as creating 2D cutouts of objects of
+interest.
+"""
---
+++ hda_fits/pink.py
@@ -0,0 +1,5 @@
+"""PINK related helper functions
+
+This module provides I/O functionality related to the PINK self
+organizing maps application. 
+"""
