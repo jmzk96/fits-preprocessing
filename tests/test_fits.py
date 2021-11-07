d15bfd3870a2da059f585fd374e117d5ad2f020d
jmzk96 <33149023+jmzk96@users.noreply.github.com>
2021-11-07 20:46:45 +0100

    corrected hda_fits to hfits, added two test functions for write_pink_file_v2_header/data

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -1,7 +1,9 @@
 import pytest
 
-from hda_fits import fits
+from hda_fits import fits,pink
 from hda_fits.logging_config import logging
+from tests.conftest import test_data_dir
+import struct
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -38,3 +40,35 @@ def test_rectangular_cutout_creation(
     )
 
     assert cutout.data.shape == size
+
+def test_write_pink_file_v2_header(
+    test_pink_header,
+    number_of_images = 3,
+    image_height = 200 ,
+    image_width = 200,
+    overwrite = True
+):
+    pink.write_pink_file_v2_header(test_pink_header,
+    number_of_images,
+    image_height,
+    image_width,
+    overwrite)
+    with open(test_pink_header,"r+b") as g:
+        content = g.read()
+        assert content == struct.pack( "i" * 8, 2, 0, 0, 3, 0, 2, 200, 200)
+
+
+def test_write_pink_file_v2_data(
+    test_pink_data,
+    mosaic_hdu_and_wcs,
+    example_object_world_coordinates
+):  
+    hdu, wcs = mosaic_hdu_and_wcs
+    example_array = fits.create_cutout2D_as_flattened_numpy_array(hdu,
+    example_object_world_coordinates,
+    20,wcs)
+    pink.write_pink_file_v2_data(test_pink_data,example_array)
+    with open(test_pink_data,"r+b") as g:
+        content = g.read()
+        assert content == struct.pack("%sf" % example_array.size, *example_array.tolist())
+
