e743979fade5022b300d0079c01396334daf88b5
Dennis Imhof <dennis.imhof@h-da.de>
2021-11-07 20:35:38 +0100

    MOD Sorted imports of __init__.py added black compatible isort pipeline

--- hda_fits/__init__.py
+++ hda_fits/__init__.py
@@ -1,10 +1,7 @@
 from hda_fits.fits import (
-    load_mosaic,
-    read_shimwell_catalog,
     RectangleSize,
     WCSCoordinates,
+    load_mosaic,
+    read_shimwell_catalog,
 )
-
-from hda_fits.pink import (
-    write_mosaic_objects_to_pink_file_v2,
-)
+from hda_fits.pink import write_mosaic_objects_to_pink_file_v2
