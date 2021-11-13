53e94ff6ddf0e226b623f58f224e4950871cab9c
Dennis Imhof <dennis.imhof@h-da.de>
2021-11-13 11:27:51 +0100

    ADD test mosaic cutout and fixtures
    Added mosaic P218+55 center cutout of 200x200px. Modified the shimwell
    catalog subset to only contain objects of P205+55 and P218+55. Added
    fixtures for objects whose cutouts of 95x95px (PINK) are fully or
    partially contained in the test mosaic cutouts.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from astropy.io import fits
 from astropy.table import Table
 from astropy.wcs import WCS
 
+import hda_fits as hf
 from hda_fits.fits import WCSCoordinates
 from hda_fits.logging_config import logging
 
@@ -19,7 +20,12 @@ def catalog_filename():
 
 @pytest.fixture(scope="module")
 def mosaic_id():
-    return "P205+55_test_cutout"
+    return "P205+55"
+
+
+@pytest.fixture(scope="module")
+def mosaic_ids():
+    return ["P205+55", "P218+55"]
 
 
 @pytest.fixture(scope="module")
@@ -45,6 +51,13 @@ def test_data_dir():
 
 
 @pytest.fixture(scope="module")
+def test_mosaic_dir(test_data_dir):
+    path = test_data_dir / "reduced_mosaics"
+    assert path.exists()
+    return path
+
+
+@pytest.fixture(scope="module")
 def catalog_filepath(test_data_dir, catalog_filename):
     path = test_data_dir / catalog_filename
     assert path.exists()
@@ -52,8 +65,8 @@ def catalog_filepath(test_data_dir, catalog_filename):
 
 
 @pytest.fixture(scope="module")
-def mosaic_filepath(test_data_dir, mosaic_filename):
-    path = test_data_dir / mosaic_filename
+def mosaic_filepath(test_mosaic_dir, mosaic_filename):
+    path = test_mosaic_dir / mosaic_filename
     assert path.exists()
     return path
 
@@ -63,6 +76,14 @@ def shimwell_catalog(catalog_filepath):
     table = Table.read(catalog_filepath)
     assert table is not None
     return table
+
+
+@pytest.fixture(scope="module")
+def shimwell_catalog_df(catalog_filepath, shimwell_catalog):
+    catalog = hf.read_shimwell_catalog(catalog_filepath)
+    assert catalog is not None
+    assert catalog.shape == shimwell_catalog.shape
+    return catalog
 
 
 @pytest.fixture(scope="module")
@@ -96,3 +117,65 @@ def example_object_pixel_coordinates():
 @pytest.fixture(scope="module")
 def example_object_world_coordinates_outside():
     return WCSCoordinates(50, 1)
+
+
+@pytest.fixture(scope="module")
+def sources_closest_to_218_center_full_95px():
+    return [
+        "ILTJ143425.75+545756.4",
+        "ILTJ143410.87+545843.2",
+        "ILTJ143426.08+545730.5",
+    ]
+
+
+@pytest.fixture(scope="module")
+def sources_closest_to_218_center_partial_95px():
+    return [
+        "ILTJ143416.01+545700.0",
+        "ILTJ143428.48+545728.7",
+        "ILTJ143419.81+545616.1",
+    ]
+
+
+@pytest.fixture(scope="module")
+def sources_closest_to_205_center_full_95px():
+    return [
+        "ILTJ134018.41+545441.9",
+        "ILTJ134004.60+545356.5",
+        "ILTJ134006.57+545515.8",
+    ]
+
+
+@pytest.fixture(scope="module")
+def sources_p205_p218_full_95px(
+    sources_closest_to_205_center_full_95px, sources_closest_to_218_center_full_95px
+):
+    return (
+        sources_closest_to_205_center_full_95px
+        + sources_closest_to_218_center_full_95px
+    )
+
+
+@pytest.fixture(scope="module")
+def sources_p205_p218(
+    sources_p205_p218_full_95px, sources_closest_to_218_center_partial_95px
+):
+    return sources_p205_p218_full_95px + sources_closest_to_218_center_partial_95px
+
+
+@pytest.fixture(scope="module")
+def catalog_p205_p218_full_95px(sources_p205_p218_full_95px, shimwell_catalog_df):
+    catalog_subset = shimwell_catalog_df[
+        shimwell_catalog_df.Source_Name.isin(sources_p205_p218_full_95px)
+    ]
+    assert catalog_subset.shape[0] == 6
+    return catalog_subset
+
+
+@pytest.fixture(scope="module")
+def catalog_p205_p218_full_and_partial_95px(sources_p205_p218, shimwell_catalog_df):
+    catalog_subset = shimwell_catalog_df[
+        shimwell_catalog_df.Source_Name.isin(sources_p205_p218)
+    ]
+    assert catalog_subset.shape[0] == 9
+    return catalog_subset
