f3686d07c33140e13cb3be57f363161a93dc37e4
jmzk96 <33149023+jmzk96@users.noreply.github.com>
2021-12-04 00:20:32 +0100

    added codes for writing sdss header

--- hda_fits/sdss.py
+++ hda_fits/sdss.py
@@ -1,17 +1,25 @@
 import urllib
+from io import BytesIO
+from typing import List
 
+import numpy as np
+import pandas as pd
 import requests
+from astropy.io.fits.hdu.hdulist import HDUList
+from astropy.io.fits.hdu.image import PrimaryHDU
+
+from .types import WCSCoordinates
 
 
-def getSDSSfields(ra, dec, size):  # all in degree
+def getSDSSfields(coordinates: WCSCoordinates, size: float):  # all in degree
 
     fmt = "csv"
     default_url = "http://skyserver.sdss3.org/public/en/tools/search/x_sql.aspx"
     delta = 0.5 * size + 0.13
-    ra_max = ra + 1.5 * delta
-    ra_min = ra - 1.5 * delta
-    dec_max = dec + delta
-    dec_min = dec - delta
+    ra_max = coordinates.RA + 1.5 * delta
+    ra_min = coordinates.RA - 1.5 * delta
+    dec_max = coordinates.DEC + delta
+    dec_min = coordinates.DEC - delta
 
     querry = "SELECT fieldID, run, camCol, field, ra, dec, run, rerun FROM Field "
     querry += (
@@ -27,7 +35,36 @@ def getSDSSfields(ra, dec, size):  # all in degree
     params = urllib.parse.urlencode({"cmd": querry, "format": fmt})
     url_opened = urllib.request.urlopen(default_url + "?%s" % params)
     lines = url_opened.readlines()
-    return lines
+    return bytes_to_pandas(lines)
+
+
+def bytes_to_pandas(list_of_bytes: List):
+    joined_byte_list = b"".join(list_of_bytes)
+    df = pd.read_csv(BytesIO(joined_byte_list))
+    df.reset_index(inplace=True)
+    headers = df.iloc[0]
+    df_with_proper_headers = pd.DataFrame(df.values[1:], columns=headers)
+    df_with_proper_headers = df_with_proper_headers(
+        {
+            "run": int,
+            "camCol": int,
+            "field": int,
+            "ra": float,
+            "dec": float,
+            "run1": int,
+            "rerun": int,
+        }
+    )
+    return df_with_proper_headers
+
+
+def find_closest_field(SDSS_metadata_df: pd.DataFrame, coordinates: WCSCoordinates):
+    selected_field = SDSS_metadata_df.iloc[
+        (SDSS_metadata_df["ra"].sub(coordinates.ra).abs().idxmin())
+        & (SDSS_metadata_df["dec"].sub(coordinates.dec).abs().idxmin())
+    ]
+    run, camcol, field = selected_field.run, selected_field.camcol, selected_field.field
+    return (run, camcol, field)
 
 
 def getSDSSfiles(fieldInfo, band, filepath):
@@ -54,3 +91,21 @@ def getSDSSfiles(fieldInfo, band, filepath):
     with open(filepath, "wb") as f:
         content = requests.get(http).content
         f.write(content)
+
+
+def create_sdss_image_array(r_band: PrimaryHDU, g_band: PrimaryHDU, b_band: PrimaryHDU):
+    r = r_band.data
+    g = g_band.data
+    b = b_band.data
+    optical_array = r / 3 + g / 3 + b / 3
+    return optical_array
+
+
+def create_sdss_fits_file(data: np.array, filepath):
+    hdu = PrimaryHDU(data)
+    hdu = HDUList([hdu])
+    hdu.writeto(filepath)
+
+
+def write_sdss_fits_header():
+    pass
